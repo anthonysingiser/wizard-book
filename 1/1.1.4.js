7c2a32e8d32f76804b73f2ab85536db8acd08b57
Anthony <asingiser@gmail.com>
2023-02-10 16:20:28 -0500

    first commit

---
+++ 1/1.1.4.js
@@ -0,0 +1,10 @@
+/* 
+-compound function given the name 'square'
+-the function represents the operation of mulitiplying something by itself
+
+*/
+function square(x) {
+    return x * x
+}
+
+console.log(square(21))
