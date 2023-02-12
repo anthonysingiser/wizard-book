9afb2751da751bee3295b911776063741feec12d
Anthony <asingiser@gmail.com>
2023-02-12 16:03:25 -0500

    add sum_of_squares function example

--- 1/1.1.4.js
+++ 1/1.1.4.js
@@ -1,14 +1,23 @@
 /* 
 -compound function given the name 'square'
--the function represents the operation of mulitiplying something by itself
+-the function represents the operation of 
+mulitiplying something by itself */
 
-*/
 function square(x) {
     return x * x
 }
 
-console.log(square(21))
+console.log("function square return: ", square(21))
 
-// square as a building block in defining other functions
+/* example of square as a building block in 
+defining other functions: square(x) + square(y)*/
 
-square(x) + square(y)
+/*  declare a function sum_of_squares that, given 
+any two numbers as arguments, produces the sum of 
+their squares. */
+
+function sum_of_squares(x,y) {
+    return square(x) + square(y) 
+}
+ 
+console.log("function sum_of_squares return: ", sum_of_squares(4,5))
