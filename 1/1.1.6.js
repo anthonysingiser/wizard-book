d47a991356c4b878ec1ea269ee4c736dc61b4f8b
chrysanthemum <asingiser@gmail.com>
2023-02-19 16:26:38 -0500

    begin notes on section 1.1.6

---
+++ 1/1.1.6.js
@@ -0,0 +1,12 @@
+
+function abs(x) {
+    return x >= 0 ? x:-x
+}
+
+/*
+ "if x is greater than or equal to zero, return x; otherwise return -x"
+ this statement is a case analysis and can be written using a conditional expression
+ 
+ the formula for a conditional expression is:
+   predicate ? consequent-expression : alternative-expression
+*/
