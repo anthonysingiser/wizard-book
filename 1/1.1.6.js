3d9f09eccc93bb22d49de263ead9861c93af1532
chrysanthemum <asingiser@gmail.com>
2023-02-20 15:29:48 -0500

    add notes on 1.1.6

--- 1/1.1.6.js
+++ 1/1.1.6.js
@@ -2,7 +2,6 @@
 function abs(x) {
     return x >= 0 ? x:-x
 }
-
 /*
  "if x is greater than or equal to zero, return x; otherwise return -x"
  this statement is a case analysis and can be written using a conditional expression
@@ -10,3 +9,28 @@ function abs(x) {
  the formula for a conditional expression is:
    predicate ? consequent-expression : alternative-expression
 */
+
+function abs2(x) {
+    return x > 0
+        ? x
+        : x===0
+        ? 0
+        : -x    
+}
+
+/* - conditional-expression syntactic form is right-associative.
+   - the interpreter ignores spaces and line breaks
+    
+   an example of the general form of a case analysis: 
+        P1
+        ? E1
+        : P2
+        ? E2
+        ...
+        :Pn
+        ?En
+        :final-alternative-expression
+*/
+
+console.log(abs(0))
+console.log(abs2(-2))
