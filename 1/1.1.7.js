63de1a3563c5fc2d1f8ceec3253865cc2d78fe69
Anthony <asingiser@gmail.com>
2023-02-28 11:41:04 -0500

    start of notes on 1.1.7

---
+++ 1/1.1.7.js
@@ -0,0 +1,22 @@
+/*
+    Example: Square Roots by Newton's Method
+
+    -there is an important difference between mathematical functions 
+    and computer functions
+    -consider the problem of computing square roots
+
+    we can define a square-root functio as:
+    the square root of x equals y such that y is greater than or equal to
+    zero and y squared equals x.
+
+    in pseudo:
+    function sqrt(x) {
+        return the y with y >= 0 &&  square(y) === x
+    }
+
+    -mathematical functions describe properties of things (declarative knowledge)
+    -computer functions describe how to how to do something (imperative knowledge)
+*/
+
+
+
