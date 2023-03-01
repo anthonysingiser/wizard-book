4103bbd656b6499384df5dbe7c50023347d7a360
Anthony <asingiser@gmail.com>
2023-03-01 11:44:53 -0500

    add notes

--- 1/1.1.7.js
+++ 1/1.1.7.js
@@ -15,8 +15,46 @@
     }
 
     -mathematical functions describe properties of things (declarative knowledge)
-    -computer functions describe how to how to do something (imperative knowledge)
+    -computer functions describe how to do something (imperative knowledge)  
+
+
 */
 
+function abs (x){
+    return x > 0
+    ? x
+    : x === 0
+    ? 0
+    : - x
+}
 
+function square (x) {
+    return x * x
+}
 
+function average (x, y){
+    return (x + y) / 2
+}
+
+function improve(guess, x) {
+    return average(guess, x / guess)
+
+}
+
+function is_good_enough(guess, x) {
+    return abs (square(guess) - x) < 0.001
+}
+
+function sqrt_iter(guess, x) {
+    return is_good_enough(guess, x)
+    ? guess
+    : sqrt_iter(improve(guess,x), x)
+}
+
+function sqrt(x) {
+    return sqrt_iter(1,x)
+}
+
+console.log(sqrt(9)
+
+)
