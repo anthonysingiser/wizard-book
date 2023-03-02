e10ec6f0145fd09b3a222649ee674495c8f6c506
Anthony <asingiser@gmail.com>
2023-03-02 10:54:14 -0500

    finish notes on 1.1.7

--- 1/1.1.7.js
+++ 1/1.1.7.js
@@ -5,7 +5,7 @@
     and computer functions
     -consider the problem of computing square roots
 
-    we can define a square-root functio as:
+    we can define a square-root function as:
     the square root of x equals y such that y is greater than or equal to
     zero and y squared equals x.
 
@@ -32,8 +32,8 @@ function square (x) {
     return x * x
 }
 
-function average (x, y){
-    return (x + y) / 2
+function average (guess, y){
+    return (guess + y) / 2
 }
 
 function improve(guess, x) {
@@ -55,6 +55,12 @@ function sqrt(x) {
     return sqrt_iter(1,x)
 }
 
-console.log(sqrt(9)
+console.log(sqrt(9))
 
-)
+console.log(sqrt(100+37))
+
+console.log(sqrt(sqrt(2) + sqrt(3)))
+
+console.log(square(sqrt(1000)))
+
+//    the sqrt_iter function shows how iteration can be acheived using only function calls
