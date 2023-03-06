4f32269141846c13905f964aae4d4727f47c6554
chrysanthemum <asingiser@gmail.com>
2023-03-06 11:40:01 -0500

    add functions for testing

--- 1/Exercise-1.7.js
+++ 1/Exercise-1.7.js
@@ -15,12 +15,34 @@ function abs(x){
 }
 
 function square(x) {
-    return x * x
+    return (x * x)
 }
 
-function is_good_enough (guess, x) {
+function average(guess, y){
+    return (guess + y) / 2
+}
+
+function improve(guess, x) {
+    return average(guess, x / guess)
+
+}
+
+function is_good_enough(guess, x) {
     return abs(square(guess - x)) < 0.001
 }
 
-console.log(abs(square(.0003 - .0003)))
-console.log(is_good_enough(.0003 - .0003))
+function sqrt_iter(guess, x) {
+    return is_good_enough(guess, x)
+    ? guess
+    : sqrt_iter(improve(guess,x), x)
+}
+
+function sqrt(x) {
+    return sqrt_iter(1,x)
+}
+
+console.log(sqrt(4))
+/* 
+    
+*/
+
