154479d275d2825c0d8c5c525d2f3337d2890fe9
chrysanthemum <asingiser@gmail.com>
2023-03-07 11:52:57 -0500

    upate exercise

--- 1/Exercise-1.7.js
+++ 1/Exercise-1.7.js
@@ -18,8 +18,9 @@ function square(x) {
     return (x * x)
 }
 
-function average(guess, y){
-    return (guess + y) / 2
+function average(guess, x){
+    console.log((guess+x) / 2)
+    return (guess + x) / 2
 }
 
 function improve(guess, x) {
@@ -41,7 +42,25 @@ function sqrt(x) {
     return sqrt_iter(1,x)
 }
 
-console.log(sqrt(4))
+//very large number test
+//console.log(square(sqrt(90000000000000)))
+
+//very small number test
+console.log(square(sqrt(.00004)))
+
 /* 
+    To test I am using the function square to return the square of the result of the function sqrt. 
+    I did this to see how close the result of the function sqrt is to the actual sqrt of the input. 
+    I also placed a console.log statement in the function average in order to print out the improved guesses 
+    as they iterated. 
+    
+    With very large numbers (in this case the number 90,000,000,000,000) we see through
+    the logs that the sqrt_iter gets stuck on the number 9486832.980505139 as it's guess. It no longer improves
+    the number through the process of the function and continues guessing this number until the call stack size 
+    is exceeded.
+    
+    With very small numbers (in this case the number .00004) we see through the logs that once the guess reaches
+    a point that it is under the threshold of .001 of a differnce it no longer iterates, whether or not the result
+    is close to being correct.
     
 */
