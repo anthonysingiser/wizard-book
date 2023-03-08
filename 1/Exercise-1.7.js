e86f8cd46e68c5d3bf46447797cea5046677af5e
chrysanthemum <asingiser@gmail.com>
2023-03-08 11:52:55 -0500

    finish exercise

--- 1/Exercise-1.7.js
+++ 1/Exercise-1.7.js
@@ -38,15 +38,18 @@ function sqrt_iter(guess, x) {
     : sqrt_iter(improve(guess,x), x)
 }
 
-function sqrt(x) {
-    return sqrt_iter(1,x)
-}
+
 
 //very large number test
 //console.log(square(sqrt(90000000000000)))
 
 //very small number test
-console.log(square(sqrt(.00004)))
+//console.log(square(sqrt(.00004)))
+
+//percent of change in guess good_enough test
+console.log(sqrt(.00004))
+console.log(sqrt(90000000000000))
+console.log(sqrt(16))
 
 /* 
     To test I am using the function square to return the square of the result of the function sqrt. 
@@ -62,5 +65,24 @@ console.log(square(sqrt(.00004)))
     With very small numbers (in this case the number .00004) we see through the logs that once the guess reaches
     a point that it is under the threshold of .001 of a differnce it no longer iterates, whether or not the result
     is close to being correct.
+
+    -An alternative strategy for implementing is_good_enough is to watch how 'guess' changes
+    from one iteration to the next and to stop when the change is a very small fraction of the 
+    guess. Design a square-root function that uses this kind of end test. Does this work better 
+    for small and large numbers?
     
 */
+
+function is_good_enough_percent(last_guess, improved_guess){
+    return ((abs(last_guess - improved_guess) / last_guess) * 100) < .005  
+}
+
+function sqrt_iter2(guess, x) {
+    return is_good_enough_percent(guess, (improve(guess, x)))
+    ? guess
+    : sqrt_iter2(improve(guess, x), x)
+}
+
+function sqrt(x) {
+    return sqrt_iter2(1,x)
+}
