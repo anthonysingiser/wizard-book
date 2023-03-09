6c861496bbf3ebc6413d603427a424cacb5b6be4
chrysanthemum <asingiser@gmail.com>
2023-03-09 14:40:57 -0500

    finish Exercise 1.8

--- 1/Exercise-1.8.js
+++ 1/Exercise-1.8.js
@@ -2,7 +2,10 @@
     Newton's method for cube roots is based on the fact that if y is an
     approximation to the cube root of x, then a better approximation is
     given by the value:
-        (x / ((y * y) + 2y)) / 3 
+        ((x / (y * y)) + 2y) / 3 
+
+    Use this formula to implement a cube-root function
+    analagous to the square-root function.
 */
 
 function abs(x) {
@@ -13,27 +16,37 @@ function abs(x) {
     : -x
 }
 
-function dividend(guess, x) {
-    return (x / ((guess * guess) + (2 * guess)))
-}
-function improve(guess, x) {
-    return dividend(guess, x) / 3
+function cube(x){
+    return x * x * x
 }
 
-function is_good_enough(last_guess, improved_guess){
-    console.log(last_guess, improved_guess)
-    return ((abs(last_guess - improved_guess) / last_guess) * 100) < 5
+function improve(guess, x) {
+    return ((x / (guess * guess)) + (2 * guess)) / 3
+}
+
+function is_good_enough(last_guess, improved_guess) {
+    return abs(last_guess - improved_guess) < .001
 }
 
 function cbrt_iter(guess, x){
     const improved_guess = improve(guess, x)
     return is_good_enough(guess, improved_guess)
-    ? improved_guess
+    ? guess
     : cbrt_iter(improved_guess, x)
 }
 
 function cbrt(x) {
-    return cbrt_iter(x, 1)
+    return cbrt_iter(1, x)
 }
 
-console.log(cbrt(64))
+console.log(cbrt(262144))
+console.log(cbrt(.00004))
+console.log(cbrt(9000000000000))
+
+/*
+    Here is the analagous function for cube-roots. I applied the same method as the final method
+    in the last exercise, as well as similar testing. The trickiest part was converting the formula
+    with the correct order of parentheses. I had a little trouble moving too quickly in the beginning 
+    of the exercise, took a break, went back with fresh eyes, applied some log statements and saw my 
+    error in the set-up of the formula. Fortitudine Vincimus 
+*/
