570b737b80be070672aaa09a9a168fbebfb105de
chrysanthemum <asingiser@gmail.com>
2023-03-14 12:43:04 -0400

    more notes on 1.2.1

--- 1/1.2.1.js
+++ 1/1.2.1.js
@@ -5,7 +5,7 @@
             3! = 3 * (3-1) * (3-2)
         -many ways to compute this problem:
         
-        n! = n * (n-1)! 
+        ex 1. n! = n * (n-1)! 
         function:
             
             function factorial(n){
@@ -29,7 +29,7 @@
             6 * (120)
             720
         
-        running product with a counter that counts from 1 to n
+        ex. 2 running product with a counter that counts from 1 to n
         function:
             
             function factorial(n) {
@@ -55,4 +55,35 @@
             fact_iter(120, 6, 6)
             fact_iter(720, 7, 6)
         
+        -both processes:
+            *compute the same mathematical function
+            *require a number of steps propotional to n to compute n!
+            *perform the same sequence of multiplications
+        
+        -evaluating the 'shapes' of the process it is seen that  
+        they evolve differently
+        
+        example 1
+            *the shape of the first example shows us expansion followed by 
+            contraction.
+            *the process of expansion builds a chain of 'deferred operations', 
+            the contraction occurs as the operations are performed.
+            *this process is called a 'recursive process'
+            *the length of operations in the chain is proportional to n,
+            this type of process is a 'linear recursive process'
+        
+        example 2
+            *does not grow or shrink, shape of the process is a straight line
+            *fixed number of 'state variables' and a fixed rule describing 
+            how the variables should be updated from state to state and an
+            optional end test
+            *this is an 'iterative process'
+            *the number of steps for this process grows in proportion to n,
+            this is a 'linear iterative process'
+
+        
+
+        
+
+
 */
