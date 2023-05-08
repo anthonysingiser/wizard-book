df7e5aae842cb8b8695953807ef712b0d484510a
chrysanthemum <asingiser@gmail.com>
2023-05-08 12:49:47 -0400

    finish Exercise-1.34

---
+++ 1.3/Exercise-1.34.scm
@@ -0,0 +1,28 @@
+#|
+    EXERCISE 1.34
+        
+    Suppose we define the procedure:
+        (define (f g) (g 2))
+        
+    Then we have:
+
+        (f square) ; = 4
+        (f (lambda (z) (* z (+ z 1)))) ; = 6
+
+
+    What happens if we (perversely) ask the interpreter to 
+    evaluate the combination (f f)? Explain.
+
+    using the substitution model:
+
+        (f f)
+
+        (f 2) 
+
+        (2 2)
+    
+    the interpreter gives the error "the object 2 is not applicable"
+
+    The operand 4 evaluates to 2, and so does the operator f.
+    2 is not a procedure that can operate on 2 and the interpreter returns the error. 
+|#
